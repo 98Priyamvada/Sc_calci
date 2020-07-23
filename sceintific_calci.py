f4ec74743277616c32dd6ce5e13be4e0979bbbab
98Priyamvada <50589695+98Priyamvada@users.noreply.github.com>
2020-07-23 21:20:59 +0530

    Add files via upload

---
+++ sceintific_calci.py
@@ -0,0 +1,220 @@
+from tkinter import *
+from tkinter.messagebox import *
+import math as m
+
+font=('verdana',22,'bold')
+
+#important function
+def clear():
+    ex=textField.get()
+    ex=ex[0:len(ex)-1]
+    textField.delete(0, END)
+    textField.insert(0, ex)
+
+def allclr():
+    textField.delete(0, END)
+
+def click_btnfun(event):
+    global p
+    print("button clicked")
+    b=event.widget
+    text=b['text']
+    print(text)
+
+
+    if text=='=':
+        try:
+            ex=textField.get()
+            ans=eval(ex)
+            textField.delete(0,END)
+            textField.insert(0, ans)
+        except Exception as e:
+            print("error",e)
+            showerror("Error", e)
+        return
+
+    textField.insert(END,text)
+#creating a window
+window=Tk()
+window.title('calculator')
+window.geometry('510x550')
+
+#picture label
+pic=PhotoImage(file='img/icons8-calculator-24.png')
+headinglabel=Label(window,image=pic)
+headinglabel.pack(side=TOP)
+#heading label
+heading= Label(window, text='Calculator', font=font)
+heading.pack(side=TOP)
+#text field
+textField= Entry(window, font=font, justify=CENTER)
+textField.pack(side=TOP, pady=10,fill=X,padx=10)
+#buttons
+buttonFrame= Frame(window)
+buttonFrame.pack(side=TOP,padx=10)
+#adding buttons to frame
+temp=1
+for i in range(0,3):
+    for j in range(0,3):
+       btn= Button(buttonFrame, text=str(temp), font=font, width=5, activebackground='orange',
+                   activeforeground='white')
+       btn.grid(row=i, column=j, padx=2, pady=2)
+       temp+=1
+       btn.bind('<Button-1>',click_btnfun)
+
+
+btn0= Button(buttonFrame, text=0, font=font, width=5, activebackground='orange',
+             activeforeground='white')
+btn0.grid(row=3, column=0, padx=2, pady=2)
+dotbtn= Button(buttonFrame, text='.', font=font, width=5, activebackground='orange',
+               activeforeground='white')
+dotbtn.grid(row=3, column=1, padx=2, pady=2)
+eqbtn= Button(buttonFrame, text='=', font=font, width=5, activebackground='orange',
+              activeforeground='white')
+eqbtn.grid(row=3, column=2, padx=2, pady=2)
+plusbtn= Button(buttonFrame, text='+', font=font, width=5, activebackground='orange',
+                activeforeground='white')
+plusbtn.grid(row=0, column=3, padx=2, pady=2)
+minus= Button(buttonFrame, text='-', font=font, width=5, activebackground='orange',
+              activeforeground='white')
+minus.grid(row=1, column=3, padx=2, pady=2)
+mulbtn= Button(buttonFrame, text='*', font=font, width=5, activebackground='orange',
+               activeforeground='white')
+mulbtn.grid(row=2, column=3, padx=2, pady=2)
+divbtn= Button(buttonFrame, text='/', font=font, width=5, activebackground='orange',
+               activeforeground='white')
+divbtn.grid(row=3, column=3, padx=2, pady=2)
+clrbtn= Button(buttonFrame, text='<--', font=font, width=11, activebackground='orange',
+               activeforeground='white', command= clear)
+clrbtn.grid(row=4, column=0,columnspan=2, padx=5, pady=5)
+allclrbtn= Button(buttonFrame, text='AC', font=font, width=11, activebackground='orange',
+                activeforeground='white', command=allclr)
+allclrbtn.grid(row=4, column=2,columnspan=2, padx=5, pady=5)
+#binding all buttons
+btn.bind('<Button-1>',click_btnfun)
+btn0.bind('<Button-1>',click_btnfun)
+dotbtn.bind('<Button-1>',click_btnfun)
+eqbtn.bind('<Button-1>',click_btnfun)
+plusbtn.bind('<Button-1>',click_btnfun)
+minus.bind('<Button-1>',click_btnfun)
+mulbtn.bind('<Button-1>',click_btnfun)
+divbtn.bind('<Button-1>',click_btnfun)
+#####################################################################################################
+# functions 2nd video functions.......
+scFrame = Frame(window)
+
+sqrtBtn = Button(scFrame, text='√', font=font, width=5, relief='ridge', activebackground='orange',
+                 activeforeground='white')
+sqrtBtn.grid(row=0, column=0)
+
+powBtn = Button(scFrame, text='^', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+powBtn.grid(row=0, column=1)
+
+factBtn = Button(scFrame, text='x!', font=font, width=5, relief='ridge', activebackground='orange',
+                 activeforeground='white')
+factBtn.grid(row=0, column=2)
+
+radBtn = Button(scFrame, text='toRad', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+radBtn.grid(row=0, column=3)
+
+degBtn = Button(scFrame, text='toDeg', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+degBtn.grid(row=1, column=0)
+
+sinBtn = Button(scFrame, text='sinθ', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+sinBtn.grid(row=1, column=1)
+
+cosBtn = Button(scFrame, text='cosθ', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+cosBtn.grid(row=1, column=2)
+
+tanBtn = Button(scFrame, text='tanθ', font=font, width=5, relief='ridge', activebackground='orange',
+                activeforeground='white')
+tanBtn.grid(row=1, column=3)
+
+normalcalc = True
+
+
+def calculate_sc(event):
+    print('btn..')
+    btn = event.widget
+    text = btn['text']
+    print(text)
+    ex = textField.get()
+    answer = ''
+    if text == 'toDeg':
+        print("cal degree")
+        answer = str(m.degrees(float(ex)))
+
+
+    elif text == 'toRad':
+        print('radian')
+        answer = str(m.radians(float(ex)))
+
+    elif text == 'x!':
+        print("cal factorial")
+        answer = str(m.factorial(int(ex)))
+    elif text == 'sinθ':
+        print("cal sin")
+        answer = str(m.sin(m.radians(int(ex))))
+    elif text == 'cosθ':
+        answer = str(m.cos(m.radians(int(ex))))
+    elif text == 'tanθ':
+        answer = str(m.tan(m.radians(int(ex))))
+    elif text == '√':
+        print('sqrt')
+        answer = m.sqrt(int(ex))
+    elif text == '^':
+        print('pow')
+        base, pow = ex.split(',')
+        print(base)
+        print(pow)
+        answer = m.pow(int(base), int(pow))
+
+    textField.delete(0, END)
+    textField.insert(0, answer)
+
+
+def sc_click():
+    global normalcalc
+    if normalcalc:
+        # sc...
+        buttonFrame.pack_forget()
+        # add sc frame
+        scFrame.pack(side=TOP, pady=20)
+        buttonFrame.pack(side=TOP)
+        window.geometry('510x700')
+        print("show sc")
+        normalcalc = False
+    else:
+        print('show normal')
+        scFrame.pack_forget()
+        window.geometry('510x550')
+        normalcalc = True
+
+
+# end functions
+# binding sc buttons
+sqrtBtn.bind("<Button-1>", calculate_sc)
+powBtn.bind("<Button-1>", calculate_sc)
+factBtn.bind("<Button-1>", calculate_sc)
+radBtn.bind("<Button-1>", calculate_sc)
+degBtn.bind("<Button-1>", calculate_sc)
+sinBtn.bind("<Button-1>", calculate_sc)
+cosBtn.bind("<Button-1>", calculate_sc)
+tanBtn.bind("<Button-1>", calculate_sc)
+
+fontMenu = ('', 15)
+menubar = Menu(window, font=fontMenu)
+
+mode = Menu(menubar, font=fontMenu, tearoff=0)
+mode.add_checkbutton(label="Scientific Calculator", command=sc_click)
+
+menubar.add_cascade(label="Mode", menu=mode)
+
+window.config(menu=menubar)
+
+window.mainloop()
